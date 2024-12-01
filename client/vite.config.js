258ba35794081b2b1e72ea74c2e08873e20fd0b7
cc46808 <cc46808@gmail.com>
2024-12-01 17:34:26 -0500

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
       '/ws': {
-        target: 'ws://localhost:3001',
+        target: process.env.RAILWAY_STATIC_URL || 'ws://localhost:3001',
         ws: true
       }
     }
